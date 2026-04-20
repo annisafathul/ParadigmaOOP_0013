ba82c25536ddb6197d975a181dc53ba85b768f37
annisafathul <annisafathul18@gmail.com>
2026-04-20 10:36:10 +0700

    inisialisi variabel

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa
-{
-    public :
-        int nim;
-        string nama;
-        float nilai;
-    
-    void printData()
-    {
-        cout << "NIM : " << nim << endl;
-        cout << "Nama :" << nama << endl;
-        cout << "Nilai :"  << nilai << endl;
-    }
 
-}; // batas class
 
